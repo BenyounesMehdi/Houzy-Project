d75585b02922d02fb6f769e7cd31e3c8dc7b79e2
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-11-15 22:16:13 +0000

    refactor

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import { Button } from "../ui/button";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 import NavMobile from "./NavMobile";
+import { auth } from "@clerk/nextjs/server";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await auth();
+
+  console.log("current user: ", user);
+
   return (
     <nav className="container mx-auto mt-3 flex justify-around items-center">
       <Logo />
@@ -11,7 +18,13 @@ export default function Navbar() {
         <NavLinks />
       </div>
       <div className="flex justify-center items-center gap-5">
-        <Button>Sign in</Button>
+        {user ? (
+          <UserButton />
+        ) : (
+          <Link href={"/sign-in"}>
+            <Button>Sign in</Button>
+          </Link>
+        )}
 
         <NavMobile />
       </div>
