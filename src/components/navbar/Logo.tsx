54ac5cf12d17d64f6852839d0ca308143e85dc78
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-11-18 21:08:11 +0000

    Added the Link Tag

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Logo() {
   return (
-    <>
+    <Link href="/">
       <p className="italic text-3xl font-semibold">
         Hou<span className="text-primary">z</span>y
       </p>
-    </>
+    </Link>
   );
 }
