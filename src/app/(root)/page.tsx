b530a734fe532538d9ae0e21029a3bd5112bdbb0
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-11-18 21:04:05 +0000

    Added the SubmitButton component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,6 @@
 import Hero from "@/components/hero/HeroSection";
-import { auth } from "@clerk/nextjs/server";
 
 export default async function Page() {
-  const userId = await auth();
-  console.log("user id: ", userId);
   return (
     <>
       <Hero />
