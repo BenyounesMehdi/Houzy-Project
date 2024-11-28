1c763231a1d2b3f179478dccdec6387bde22ad44
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-11-28 16:37:50 +0000

    Added & Invoked the PropertyForm componen

---
+++ src/app/(root)/property/update/[id]/page.tsx
@@ -0,0 +1,21 @@
+import PropertyForm from "@/components/property/PropertyForm";
+import { updateProperty } from "@/utils/actions/update-property";
+import { getProperty } from "@/utils/data/property/get-property";
+import { Property } from "@/utils/types/types";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  const data = await getProperty(id);
+  const property = data as Property;
+  return (
+    <PropertyForm
+      property={property}
+      submitButtonLabel="Update property"
+      actionName="update"
+    />
+  );
+}
