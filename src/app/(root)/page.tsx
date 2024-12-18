0c656e83b7d5d7155afc7b9f4abe2d93e75aed12
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-12-18 19:36:31 +0000

    Added the WhyHouzyChoose component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero/HeroSection";
+import WhyChooseHouzy from "@/components/landing/WhyChooseHouzySection";
 
 export default async function Page() {
   return (
     <>
       <Hero />
+      <WhyChooseHouzy />
     </>
   );
 }
