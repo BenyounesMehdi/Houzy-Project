3ec9e7bfb5335f23b06aed0f210ea36a99d5f32d
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-12-18 19:05:49 +0000

    Removed the Navbar component

--- src/app/(root)/sell/layout.tsx
+++ src/app/(root)/sell/layout.tsx
@@ -4,7 +4,6 @@ import Provider from "@/utils/providers/Provider";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Navbar />
       <Provider>{children}</Provider>
     </>
   );
