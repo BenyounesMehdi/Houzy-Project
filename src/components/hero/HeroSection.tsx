98ac634f70cfe6ee251648f63e94d12e2900b58b
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-12-18 20:21:27 +0000

    Updated the ui

--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
     <div className="container mx-auto mt-10 flex flex-col justify-center items-center gap-3 text-center">
       <p className="font-bold text-4xl sm:text-5xl">Find Your Dream</p>
       <p className="font-bold text-4xl sm:text-5xl text-primary">Home Today</p>
-      <p className="w-full sm:w-3/4 text-muted-foreground font-semibold text-sm sm:text-base">
+      <p className="w-full sm:w-3/4 text-muted-foreground font-semibold text-sm sm:text-base px-2">
         Welcome to <span className="text-primary">Houzy</span> your one-stop
         solution for buying, renting, or selling properties. Easily browse
         listings, upload your own property, and connect with buyers or renters.
