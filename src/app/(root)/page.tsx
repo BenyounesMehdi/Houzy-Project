c1f1bbf37ac8599f53647a642e4f67e139cebbb8
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-12-18 20:24:09 +0000

    Added FeaturedProperties component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/hero/HeroSection";
 import WhyChooseHouzy from "@/components/landing/WhyChooseHouzySection";
+import FeaturedProperties from "@/components/property/FeaturedProperties";
 
 export default async function Page() {
   return (
     <>
       <Hero />
+      <FeaturedProperties />
       <WhyChooseHouzy />
     </>
   );
