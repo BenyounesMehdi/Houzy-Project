10969026c0092f476eb4afb92de882d78ac4970b
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-12-19 20:39:46 +0000

    Removed the auth conditions

--- src/utils/data/property/get-top-properties.ts
+++ src/utils/data/property/get-top-properties.ts
@@ -1,15 +1,8 @@
 "server only";
 
 import prisma from "@/lib/db";
-import { auth, clerkClient } from "@clerk/nextjs/server";
 
 export const getTopProperies = async (propertyType: string) => {
-  const { userId } = await auth();
-  const response = await (await clerkClient()).users.getUser(userId as string);
-
-  if (!response.id) {
-    return null;
-  }
   try {
     const data = await prisma.property.findMany({
       select: {
